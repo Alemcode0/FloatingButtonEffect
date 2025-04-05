00794a4dc534d78a20feba1ea70870c52815e6cd
alemayehu <alemcode0@gmail.com>
2025-04-05 17:00:38 +0200

    Update 2

--- FloatingButtonEffect/FloatingButtonEffectApp.swift
+++ FloatingButtonEffect/FloatingButtonEffectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FloatingButtonEffectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FloatingButton()
         }
     }
 }
