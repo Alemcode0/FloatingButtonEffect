97da87cd86566595e24bf7f978ac4429f070fa6a
alemayehu <alemcode0@gmail.com>
2025-04-05 16:47:54 +0200

    Initial Commit

---
+++ FloatingButtonEffect/FloatingButtonEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonEffectApp.swift
+//  FloatingButtonEffect
+//
+//  Created by Alemayehu Hailye on 05.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
